949e0eda80f4ae6352150b29f87031286f44635b
SosisMayo <ramadani.dwicahya@gmail.com>
2021-09-27 09:17:02 +0700

    Menambahkan fungsi hubungkan dan melakukan beberapa perbaikan

--- TugasArray2/tugas2/main.cpp
+++ TugasArray2/tugas2/main.cpp
@@ -7,39 +7,60 @@ class CKota{
 public:
     string nama;
     float x,y;
-    int node1,node2;
+    int tujuanKiri = -1, tujuanKanan = -1;
 };CKota Kota[100];
 
-void tambah(CKota Kota[100], int *M, string nama, int x, int y, int node1, int node2){
+void tambah(int *M, string nama, int x, int y){
     int N = 100;
     if (*M < N - 1){
         *M = *M + 1;
         Kota[*M].nama = nama;
         Kota[*M].x = x;
         Kota[*M].y = y;
-        Kota[*M].node1 = node1;
-        Kota[*M].node2 = node2;
     }
 }
 
-void cari(CKota Kota[100],int M,string nama){
-    int posisi=-1;
+void cari(int M,string nama,int *posisi){
+    *posisi = -1;
     for(int i=0; i<=M; i++){
         if(Kota[i].nama == nama){
-            posisi = i;
+            *posisi = i;
+            break;
         }
     }
-    cout << posisi << endl;
+}
+
+void hubungkan(string asal,string tujuanKiri,string tujuanKanan,int M){
+    int indexAsal,indexTujuanKiri,indexTujuanKanan;
+    cari(M,asal,&indexAsal);
+    cari(M,tujuanKiri,&indexTujuanKiri);
+    cari(M,tujuanKanan,&indexTujuanKanan);
+    Kota[indexAsal].tujuanKiri = indexTujuanKiri;
+    Kota[indexAsal].tujuanKanan = indexTujuanKanan;
 }
 
 int main()
 {
-    int M = -1;
-    tambah(Kota,&M,"A",0,0,2,3);
-    tambah(Kota,&M,"B",1,1,4,5);
-    tambah(Kota,&M,"D",1,-1,4,5);
-    tambah(Kota,&M,"F",2,1,2,3);
-    tambah(Kota,&M,"E",3,-1,2,3);
-    cari(Kota,M,"E");
+    // M sebagai penunjuk akhir array, posisi untuk menyimpan index yang dicari
+    int M = -1,posisi;
+
+    // MENAMBAHKAN DATA KOTA
+    tambah(&M,"A",0,0);
+    tambah(&M,"B",1,1);
+    tambah(&M,"D",1,-1);
+    tambah(&M,"F",2,1);
+    tambah(&M,"E",3,-1);
+
+    //MENGHUBUNGKAN KOTA SATU DENGAN KOTA LAINNYA
+    hubungkan("A","B","D",M);
+    hubungkan("B","F","E",M);
+    hubungkan("D","F","E",M);
+    cout << "Kota " << Kota[0].nama << " berhubungan dengan Kota " << Kota[Kota[0].tujuanKiri].nama << " dan Kota " << Kota[Kota[0].tujuanKanan].nama << endl;
+    cout << "Kota " << Kota[1].nama << " berhubungan dengan Kota " << Kota[Kota[1].tujuanKiri].nama << " dan Kota " << Kota[Kota[1].tujuanKanan].nama << endl;
+    cout << "Kota " << Kota[2].nama << " berhubungan dengan Kota " << Kota[Kota[2].tujuanKiri].nama << " dan Kota " << Kota[Kota[2].tujuanKanan].nama << endl;
+
+    //MENCARI DATA KOTA MELALUI NAMA KOTA
+    cari(M,"E",&posisi);
+    cout << "Kota E berada pada index " << posisi <<endl;
     return 0;
 }
